983f80c29b957de308287594f466c727c1a45321
devu0508 <divyanshupadhyay23aiml@rnsit.ac.in>
2025-10-07 03:49:50 +0530

    selectionsort

--- Array.cpp/swap01.cpp
+++ Array.cpp/swap01.cpp
@@ -42,3 +42,54 @@ int main(){
  printArray(arr, 8);
   return 0;
 }
+
+
+
+
+
+// bool isPosssible(vector<int>& position, int m,int mid){
+
+//     int basketCount = 1;
+//     int lastPos = position[0];
+
+//     for(int i = 0; i<position.size(); i++){
+//         if(position[i]-lastPos >=mid){
+//             basketCount++;
+//             if(basketCount==m){
+//                 return true;
+//             }
+//             lastPos = position[i];
+//         }
+//     }
+//     return false;
+// }
+// class Solution {
+// public:
+//     int maxDistance(vector<int>& position, int m) {
+//         sort(position.begin(), position.end());
+//         int s = 0;
+//         int maxi = -1;
+//         for(int i = 0;i<position.size();i++){
+//             maxi = max(maxi, position[i]);
+//         }
+//         int e = maxi;
+//          int ans = -1;
+//         int mid = s + (e-s)/2;
+
+//         while(s<=e){
+//             if(isPosssible(position,m,mid)){
+
+//                 ans = mid;
+//                 s = mid+1;
+
+//             }
+//             else{
+//                 e = mid-1;
+//             }
+//             mid = s + (e-s)/2;
+//         }
+//         return ans;
+
+        
+//     }
+// };
