e74fa3dd696782d08e6f69841e33202336f4a2c9
devu0508 <divyanshupadhyay23aiml@rnsit.ac.in>
2025-10-07 03:50:13 +0530

    Selection_sort

---
+++ Array.cpp/Selection_sort.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int arr[5] = {5, 2, 9, 1, 5};
+
+    int n=5;
+    for(int i=0; i<n-1; i++){
+        int minIndex = i;
+        for(int j=i+1; j<n; j++){
+            if(arr[j]<arr[minIndex]){
+                minIndex = j;
+            }
+        }
+        swap(arr[i], arr[minIndex]);
+    }
+
+    for(int i=0; i<n; i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+  return 0;
+}
